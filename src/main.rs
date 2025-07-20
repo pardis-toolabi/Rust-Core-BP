e873eb99b188f71bd1a76ea3fbb5e824cee85cdc
pardis-toolabi <pardis.tlbi@gmail.com>
2025-07-20 12:25:36 +0200

    adding health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, get, post, web};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(health_check))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
+
+#[get("/healthCheck")]
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
