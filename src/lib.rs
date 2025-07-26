0bdeeea9b144e254956fb31273e30c92ea07f62a
pardis-toolabi <pardis.tlbi@gmail.com>
2025-07-27 00:07:26 +0200

    adding subs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, get};
+use actix_web::{dev::Server, get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
 pub fn run() -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| App::new().service(health_check))
@@ -11,3 +12,14 @@ pub fn run() -> Result<Server, std::io::Error> {
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
+
+#[derive(Serialize, Deserialize)]
+struct FormData{
+    name: String,
+    email: String
+}
+#[post("/subscriptions")]
+async fn subscriptions(web::Form(form): web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
